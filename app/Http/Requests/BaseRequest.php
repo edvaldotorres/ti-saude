1c0bd2dbe7be93676ceec63214362bd27395aa21
edvaldotorres <edvaldodev@gmail.com>
2022-06-14 00:07:11 -0300

    feat: bases

---
+++ app/Http/Requests/BaseRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\HttpFoundation\Response;
+
+class BaseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'required' => 'O campo :attribute é obrigatório', 'numeric',
+            'string' => 'O campo :attribute deve ser uma string',
+            'max' => 'O campo :attribute deve ter no máximo :max caracteres',
+            'date' => 'O campo :attribute deve ser uma data válida',
+            'date_format' => 'O campo :attribute deve ser uma data válida',
+            'unique' => 'O campo :attribute já existe',
+        ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function failedValidation($validator): void
+    {
+        throw new HttpResponseException(response()->json([
+            'messenge'   => current($validator->errors()->all()),
+        ], Response::HTTP_BAD_REQUEST));
+    }
+}
---
+++ app/Models/BaseModel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseModel extends Model
+{
+    use HasFactory;
+
+    /**
+     * Accessors & Mutators
+     */
+    protected function convertStringToDate(?string $param)
+    {
+        if (empty($param)) {
+            return null;
+        }
+
+        list($day, $month, $year) = explode('/', $param);
+        return (new \DateTime($year . '-' . $month . '-' . $day))->format('Y-m-d');
+    }
+}
